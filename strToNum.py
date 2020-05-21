c40ef46d945c9834e6fb4074d9506c1cd1aa5d11
axaysushir <sinewave_11@yahoo.com>
2020-05-21 16:57:56 +0530

    add description

--- strToNum.py
+++ strToNum.py
@@ -1,12 +1,19 @@
 # convert string into numbers
 # ex: "1234 Hello"  res: 1234
+# step 1: check is str or remove whitespace using strip if not return 0
+# step 2: check sign is '+' or '-' set the sign
+# step 3: Traverse througn string from start to string of the length
+# step 4: Check each character if is numeric then return sign * res, convert res to str
+# step 5: sign + or - and range 2 ^31 to 2 ^ 31 - 1 if res out of range return this val * sign
+
 str = "1234 Hello"
+
 
 class Solution:
     def myAtoi(self, s: str) -> int:
         if not s:
             return 0
-        s = s.strip() # RETURN COPY OF THE STRING WITH LEADING AND TRAILING WHITE SPACES
+        s = s.strip()  # RETURN COPY OF THE STRING WITH LEADING AND TRAILING WHITE SPACES
         if not s:
             return 0
 
@@ -22,8 +29,8 @@ class Solution:
         for i in range(start, len(s)):
             if not s[i].isnumeric():
                 return sign * res
-            res = res * 10 + int(s[i])
-            if sign == 1 and res > 2 ** 31 -1:
+            res = res * 10 + int(s[i])  # convert str to integer
+            if sign == 1 and res > 2 ** 31 - 1:  # check range 2^31 -1 to 2^31
                 return 2 ** 31 - 1
             elif sign == -1 and res >= 2 ** 31:
                 return -2 ** 31
