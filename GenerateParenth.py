f714c1406a16e63375aacc889cbcdd3123beac80
axaysushir <sinewave_11@yahoo.com>
2020-05-22 22:12:02 +0530

    backtrack solution

--- GenerateParenth.py
+++ GenerateParenth.py
@@ -38,3 +38,21 @@ class Solution:
         ans = []
         generate()
         return ans
+
+# backtracking
+
+
+class Solution:
+    def backtrackParenthesis(self, n):
+        ans = []
+
+        def backtrack(S='', left=0, right=0):
+            if len(S) == 2 * n:
+                ans.append(S)
+                return
+            if left < n:
+                backtrack(S+'(', left + 1, right)
+            if right < left:
+                backtrack(S+')', left, right+1)
+        backtrack()
+        return ans
