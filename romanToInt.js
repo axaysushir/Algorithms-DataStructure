5fbf4f67c50b742bebc20670828cfed2981709f1
axaysushir <sinewave_11@yahoo.com>
2020-08-12 18:32:06 +0530

    Roman to Integer

--- romanToInt.js
+++ romanToInt.js
@@ -1,5 +1,32 @@
-//  convert roma nums to integer
+/*  convert roma nums to integer
+Given a Roman numeral, find the corresponding decimal value. Inputs will be between 1 and 3999. 
+Example: 
+Input: IX
+Output: 9
+
+Input: VII
+Output: 7
+
+Input: MCMIV
+Output: 1904
+Roman numerals are based on the following symbols: 
+I     1
+IV    4
+V     5
+IX    9 
+X     10
+XL    40
+L     50
+XC    90
+C     100
+CD    400
+D     500
+CM    900
+M     1000
+*/
+
 const conversion = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
 var romanToInt = function(s) {
     const array = s.split('');
     let total = 0
