11b38f64e59a7bb2ad8ace2cb2841628f50e376c
axaysushir <sinewave_11@yahoo.com>
2020-10-21 20:20:13 +0530

    anagram

---
+++ anagramInString.py
@@ -0,0 +1,49 @@
+# Given 2 strings s and p, find and return all indexes in string s where t is an anagram.
+from collections import Counter
+import string
+
+# Solution 1: 128ms
+def alphabetCounter(s):
+    counter = Counter(s)
+    for c in string.ascii_lowercase:
+        if c not in counter:
+            counter[c] = 0
+    return counter
+
+def findAnagram(s, t):
+    l = []
+    if len(s) < len(t):
+        return l
+    histogram = alphabetCounter(t)
+    # current is moving window of histogram
+    current = alphabetCounter(s[0:len(t)])
+    for i in range(len(s) - len(t) + 1):
+        # compare cur to histo
+        if histogram == current:
+            l.append(i)
+        # if last iteration stop
+        if i == len(s) - len(t):
+            break
+        # advance current
+        current[s[i]] -= 1
+        current[s[i + len(t)]] += 1
+    
+    return l
+
+print(findAnagram('cbaebabacd', 'abc'))
+
+
+# Solution 2:
+def find_anagrams(s, p):
+    l = len(p)
+    res = []
+    c = Counter(p)
+    for i in range(len(s) - l+1):
+        tmp = s[i:i+l]
+        if c == Counter(tmp):
+            res.append(i)
+    
+    return res
+
+print(find_anagrams('cbaebabacd', 'abc'))
+# [3, 7]
