8937e3eac64a7bc3a81ec33d67440ee2200e6b7f
axaysushir <sinewave_11@yahoo.com>
2020-11-14 11:49:57 +0530

    decimal to hexa

---
+++ decimalToHexa.py
@@ -0,0 +1,31 @@
+# Given a non-negative integer n, convert the integer to hexadecimal and return the result as a string. Hexadecimal is a base 16 representation of a number, where the digits are 0123456789ABCDEF. Do not use any builtin base conversion functions like hex.
+
+def to_hex(n):
+    # char array to store hex num
+    hexa = ['0'] * 100
+
+    # counter for hexa num array
+    i = 0
+    while n != 0:
+        # temp variable to store remainder
+        temp = 0
+        temp = n % 16 # storing remainder in temp variable
+
+        # check is temp if temp < 10
+        if (temp < 10):
+            hexa[i] = chr(temp + 48)
+            i = i + 1
+        else:
+            hexa[i] = chr(temp + 55)
+            i = i+1
+        n = int(n / 16)
+    
+    # printing hexa num array in reverse order
+    j = i - 1
+    while j >= 0:
+        print((hexa[j]), end = ' ')
+        j = j - 1
+
+# driver code
+to_hex(123)
+# 7B
