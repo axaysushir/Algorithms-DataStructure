db4300e04fb3b821994c967579fe3c32164cbe9f
axaysushir <akshay.sushir99@gmail.com>
2021-01-21 11:14:50 +0530

    bst

--- rough.py
+++ rough.py
@@ -274,3 +274,11 @@ class Node:
             return True
         
         return findtree(t.left, s) or findtree(t.right, s)
+
+# Code runner 
+t3 = Node(4, Node(3), Node(2))
+t2 = Node(5, Node(4), Node(-1))
+t = Node(1, t2, t3)
+
+s = Node(4, Node(3), Node(2))
+print(findtree(s, t))
