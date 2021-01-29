5edfd88cd86becd7579c26607b321edc3bf6cab6
axaysushir <akshay.sushir99@gmail.com>
2021-01-29 19:03:55 +0530

    add file

--- algos.js
+++ algos.js
@@ -441,3 +441,18 @@ function logest(str) {
   }
   return prefix
 }
+
+const length =(s) => {
+  let longest = 0
+  let start = 0
+  const seen = {}
+
+  [...s].forEach((char, i) => {
+    if (char in seen && start <= seen[char]) {
+      longest  = Math.max(i-start, longest)
+      start = seen[char] + 1
+    }   
+    seen[char] = i
+  });
+  return Math.max(s.length - start, longest)
+}
