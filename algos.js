7d0879918ea7d469aa9e3451ad54e399fcbb919a
axaysushir <akshay.sushir99@gmail.com>
2021-02-07 13:16:26 +0530

    basic calculator

--- algos.js
+++ algos.js
@@ -596,3 +596,39 @@ var getcloned = (original, cloned, target) => {
   if (original.val == target.val) return cloned
   else return getcloned(original.left, cloned.left, target)
 }
+
+var y = '3'
+var x = Number(y)
+x =+ y
+x += y
+console.log(x, y);
+console.log(`${typeof x}, ${typeof y}`);
+
+// Basic calculator
+var calculator = function(s) {
+  let res = 0, sign = 1, stack = [], signStack = []
+
+  for (let i=0; i < s.length; i++) {
+    if (s[i] === '-') sign = -1
+    else if (s[i] === '+') sign = 1
+    else if (s[i] >= '0' && s[i] <= '9') {
+      var num = s[i]
+      while (i+1 < s.length && s[i+1] >= '0' && s[i+1] <= '9'){
+        num += s[i+1]
+        i++
+      }
+      res += sign * parseInt(num)
+    } else if (s[i] == '(') {
+      stack.push(res)
+      signStack.push(sign)
+      res = 0
+      sign = 1
+    } else if (s[i] == ')') {
+      res = stack.pop() + signStack.pop() * res
+    }
+  }
+  return res
+}
+
+let s = '- (323 + (-123 + 12) - ( 2 - 1 ) )'
+console.log(calculator(s));
