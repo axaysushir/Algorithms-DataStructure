10d902b4152291df2ff069c10ce1af9a83db71c0
axaysushir <akshay.sushir99@gmail.com>
2021-03-12 11:57:44 +0530

    Find Cycle in Graph

--- rough.py
+++ rough.py
@@ -965,7 +965,9 @@ class Graph:
 g = Graph(-1)
 g.addEdge(0, 1)
 g.addEdge(1, 2)
-g.addEdge(2, 3)
+# g.addEdge(2, 3)
 g.addEdge(0, 2)
+g.addEdge(0, 3)
+g.addEdge(3, 4)
 
 print(g.isCycle())               
