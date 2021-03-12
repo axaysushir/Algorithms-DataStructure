46855c0bfa8ddde3b65236248a23ccb7a7e4bd3e
axaysushir <akshay.sushir99@gmail.com>
2021-03-12 11:45:42 +0530

    Find Cycle in Graph

--- rough.py
+++ rough.py
@@ -929,3 +929,43 @@ def pow(x, y):
 
 print(pow(567889, 125985849484943))
 print(pow(2345, 12456))
+
+
+# Find cycle in graph
+from collections import defaultdict
+class Graph:
+    def __init__(self, vertices):
+        self.v = vertices
+        self.graph = defaultdict(list)
+    
+    # Add edges to graph
+    def addEdge(self, v, w):
+        self.graph[v].append(w)
+        self.graph[w].append(v)
+
+    def isCyclicUtil(self, v, visited, parent):
+        visited[v] = True
+
+        for i in self.graph[v]:
+            if visited[i] == False:
+                if (self.isCyclicUtil(i, visited, v)):
+                    return True
+                elif parent != i:
+                    return True
+        return False
+    
+    def isCycle(self):
+        visited = [False]*(self.v)
+        for i in range(self.v):
+            if visited[i] == False:
+                if (self.isCyclicUtil(i, visited, -1)) == True:
+                    return True
+        return False
+
+g = Graph(-1)
+g.addEdge(0, 1)
+g.addEdge(1, 2)
+g.addEdge(2, 3)
+g.addEdge(0, 2)
+
+print(g.isCycle())               
