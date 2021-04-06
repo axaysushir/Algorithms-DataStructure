303a53727ecc409eaed185e4e378c243cb6a8f06
axaysushir <akshay.sushir99@gmail.com>
2021-04-06 17:16:28 +0530

    3 sum

--- rough.py
+++ rough.py
@@ -1296,3 +1296,21 @@ class Solution:
 
 print(Solution().balbst([1,2,3,4]))
 
+
+def closeSum(nums, target):
+    diff = float('inf')
+    nums.sort()
+    for i in range(len(nums)):
+        lo, hi = i+1, len(nums) - 1
+        while lo > hi:
+            sum = nums[i] + nums[lo] + nums[hi]
+            if abs(target-sum) < abs(diff):
+                diff = target - sum
+            if sum < target:
+                lo += 1
+            else: hi -= 1
+        if diff == 0:
+            break
+    return target - diff
+
+print(closeSum([2, 1, -5, 4], 3))
