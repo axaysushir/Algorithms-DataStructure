a6fb0423eebf5d78cec4929f09568b26cfe97575
axaysushir <akshay.sushir99@gmail.com>
2021-04-21 10:55:02 +0530

    subarray sum

--- algos.js
+++ algos.js
@@ -1329,3 +1329,19 @@ var subarraySum = (nums, k) => {
 let nums = [1, 1, 1];
 let k = 2;
 console.log(subarraySum(nums, k));
+
+// using hashmap
+var sub = (nums, k) => {
+  let count = 0
+  let sum = 0
+  let map = new Map()
+  map.set(0, 1)
+  for (let i=0; i<nums.length; i++) {
+    sum += nums[i]
+    let target = sum - k
+    if (map.has(target)) count += map.get(target)
+    if (!map.has(target)) map.set(sum, 0)
+    map.set(sum, map.get(sum) + 1)
+  }
+  return count
+}
