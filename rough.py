d9bbb68cc8d097eeae072f7b9a4fdfbaabe2033e
axaysushir <akshay.sushir99@gmail.com>
2021-06-28 16:14:50 +0530

    max path sum

--- rough.py
+++ rough.py
@@ -1753,3 +1753,36 @@ class Solution:
 
         nodeX, nodeY = res
         return nodeX[0] != nodeY[0] and nodeX[1] == nodeY[1]
+
+
+# find max path sum:
+class Node:
+    def __init__(self, val=0, left = None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution():
+    def maxpathsum(self, root):
+        self.max = float('-inf')
+
+        def visit(node):
+            if node is not None:
+                return 0
+            
+            left = visit(node.left)
+            right = visit(node.right)
+            currmax = max(node.val + max(left, right), node.val)
+            self.max = max(self.max, currmax, left + right+node.val)
+            return currmax
+        
+        visit(root)
+        return self.max
+
+n6 = Node(6)
+n4 = Node(4)
+n3 = Node(3, None, n4)
+n2 = Node(2, None, n6)
+n1 = Node(1, n2, n3)
+
+print(Solution().maxpathsum(n1))
