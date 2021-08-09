383155c9bdc8a380fb8f6a0931555971cc2922c7
axaysushir <akshay.sushir99@gmail.com>
2021-08-09 12:00:00 -0600

    kth smallest in bst

---
+++ kthSmallestInBST.js
@@ -0,0 +1,43 @@
+// Leetcode: 230 - Kth smallest element in BST
+// Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+var kthsmall = (root, k) =>{ 
+    let output = []
+    inorder(root)
+    return output[k-1]
+
+    function inorder(root) {
+        if (root === null) return;
+        inorder(root.left)
+        nums.push(root.val)
+        inorder(root.right)
+    }   
+}
+
+root = TreeNode(3)
+root.left = TreeNode(1)
+root.left.right = TreeNode(2)
+root.right = TreeNode(4)
+k = 1
+console.log(kthsmall(root, k));
+
+// sol -2 
+var kthSmallest = function(root, k) {
+    let count = 0;
+    let nums = []
+
+    while (nums.length > 0 || root !== null) {
+       while(root !== null) {
+        nums.push(root)
+        root = root.left
+        }
+    }
+    root = nums.pop()
+    count++;
+    if (count === k) return root.val;
+    root = root.right;
+};
