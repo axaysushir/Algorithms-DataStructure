6fa6d1de614687bf263f7a6bd72ae31f9308c536
axaysushir <akshay.sushir99@gmail.com>
2021-10-20 13:00:00 -0600

    range sum

--- rangeSumOfBST.js
+++ rangeSumOfBST.js
@@ -1,10 +1,15 @@
+// Range sum of BST.
+// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+// Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+// Output: 32
+// Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+
 var rangeSumBST = function(root, low, high) {
     if (root == null) return 
-    let arr = []
-    arr.push(root)
+    let arr = [root]
     let sum = 0
     
-    while(!arr.length) {
+    while(arr.length) {
         current = arr.shift()
         if (current.val >= low && current.val <= high) {
             sum += current.val;
