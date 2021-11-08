d00ad59d4f203206c4484f2cf78e253703153eb5
axaysushir <akshay.sushir99@gmail.com>
2021-11-08 14:33:59 +0530

    Reverse string

---
+++ ReverseString.js
@@ -0,0 +1,11 @@
+var reverseString = s => {
+    if(s.length === 1) return s;
+    let temp;
+    for (let i=0, j=s.length-1; i<s.length/2; i++, j--){
+        temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+    }
+}
+
+s = ['h', 'e', 'l', 'l', 'o']
