ad64f335ca14724b2e89dad87556b95420e2c49e
axaysushir <akshay.sushir99@gmail.com>
2021-11-24 15:55:00 -0600

    Max are of rect

--- MaxAreaofRect.js
+++ MaxAreaofRect.js
@@ -25,3 +25,23 @@
 
 // Input: matrix = [["1"]]
 // Output: 1
+
+// call size exceeded
+var maximalRectangle = function(matrix) {
+    let maxarea = 0
+    for (let i=0; i<matrix.length; i++) {
+        for (let j=0; j<matrix[i].length; j++) {
+            if (matrix[i][j] == '1') {
+                let temp = explore(matrix, i, j)
+                maxarea = Math.max(maxarea, temp)
+            }
+        }
+    }
+    return maxarea
+};
+
+const explore = (grid, i, j) => {
+    if (i<0 || i>= grid.length || j < 0 || j > grid[i].length || grid[i][j] == '0') return 0
+    grid[i][j] = '0'
+    return 1+ explore(grid, i-1, j) + explore(grid, i+1, j) + explore(grid, i, j-1) + explore(grid, i, j+1)
+}
