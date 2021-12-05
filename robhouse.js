dba7469f3cc612f488251e46926bb73f5b66c692
axaysushir <akshay.sushir99@gmail.com>
2021-12-05 13:08:31 +0530

    Rob House

--- robhouse.js
+++ robhouse.js
@@ -9,7 +9,23 @@
 
 // Example 1:
 
-// Input: nums = [1,2,3,1]
+// Input: 
+nums = [1,2,3,1]
 // Output: 4
 // Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 // Total amount you can rob = 1 + 3 = 4.
+
+var rob = nums =>{ 
+    if (nums === null || !nums.length) return 0
+    if (nums.length == 1) return nums[0]
+    if (nums.length == 2) return Math.max(nums[0], nums[1])
+    let dp = new Array(nums.length)
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+    for (let i=2; i<dp.length; i++) {
+        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1])
+    }
+    return dp[nums.length-1]
+}
+
+console.log(rob(nums))
