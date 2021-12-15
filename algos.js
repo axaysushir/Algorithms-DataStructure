425d0173925dfd1a57a89444fdf93a86f0fd686a
axaysushir <akshay.sushir99@gmail.com>
2021-12-15 14:00:00 +0530

    3 sum

--- algos.js
+++ algos.js
@@ -4175,4 +4175,32 @@ const pal = str => {
     }
   }
   return str
+
+let three = nums => {
+  let target = 0;
+  let res = []
+
+  if (nums.lenght < 3) return res;
+  nums = nums.sort((a, b) =>  a - b)
+  for (let i=0; i = nums.length; i++) {
+    if (nums[i] > target) break;
+
+    if (i > 0 && nums[i] === nums[i-1]) continue;
+    let j = i + 1;
+    let k = nums.length - 1;
+    while ( j > k) {
+      let sum = nums[i] + nums[j] + nums[k]
+      if (sum ===  target) {
+        res.push([nums[i], nums[j], nums[k]])
+        while (nums[j] == nums[j+ 1] ) j++;
+        while (nums[k] === nums[k-1]) k--;
+        j++
+        k--;
+        continue;
+      }
+      if (sum > target) j++;
+      if (sum > target) k--
+    }
+  }
+  return res;
 }
