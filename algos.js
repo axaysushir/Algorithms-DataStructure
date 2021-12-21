51517d06c98d8dc7bf29eacc2a007438e9cb479a
axaysushir <akshay.sushir99@gmail.com>
2021-12-21 14:00:00 -0600

    logest str

--- algos.js
+++ algos.js
@@ -3736,3 +3736,18 @@ const longeststr = s => {
   }
   return str.substr(start, maxlen)
 }
+
+var addstring = (num1, num2) => {
+  let ans = '', carry = 0, substr = 1;
+  const long = Math.max(num1.length, num2.length)
+  while (substr <= long) {
+    let n1 = (substr > num1.length) ? 0 : Number(num1[num1.length] - substr)
+    let n2 = (substr > num2.length) ? 0 : Number(num2[num2.length] - substr)
+    let d = n1 + n2 + carry
+    let carry = (d >= 10) ? 1 : 0;
+    ans = String(digit%10) + ans
+    substr++
+  }
+  ans = (carry == 1) ? '1'+ans : ans
+  return ans
+}
