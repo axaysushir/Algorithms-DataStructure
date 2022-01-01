f80ad66f0a02c532e9fb64978c20408d9ae3ed1d
axaysushir <akshay.sushir99@gmail.com>
2022-01-01 15:00:00 -0600

    Duplicate node

--- algos.js
+++ algos.js
@@ -3851,3 +3851,21 @@ let deleteNode = head => {
     return node.next
   }
 }
+
+let duplicate = head => {
+  if (!head) return null
+  let pre = new ListNode(0)
+  pre.next = head;
+  let node = pre;
+
+  while (node.next && node.next.next) {
+    if (node.next.val === node.next.next.val) {
+      let val = node.next.val;
+      while (node.next && node.next.val == val){
+        node.next = node.next.next
+      }
+    }
+    else node = node.next
+  }
+  return pre.next
+}
