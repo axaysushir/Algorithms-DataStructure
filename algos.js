4fb57415f545ac7bcef3b6bf967d6aa4666d771a
axaysushir <akshay.sushir99@gmail.com>
2022-01-28 16:00:03 -0600

    Intersect Node

--- algos.js
+++ algos.js
@@ -4078,3 +4078,16 @@ const invert = root => {
   root.right = left;
   return root;
 }
+
+get IntersectionNode = (headA, headB) => {
+  if (headA == null || headb == null) return null;
+  let p1 = headA, p2 = headB;
+  while (p1 !== p2) {
+    p1 = p1.next;
+    p2 = p2.next;
+    if (p1 == p2) return p1;
+    if (p1 == null) p1 = headB;
+    if (p2 == null) p2 = headA
+  }
+  return p1;
+}
