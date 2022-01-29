076f79c9065d80c5dcb8bef36fa0b85348d86b36
axaysushir <akshay.sushir99@gmail.com>
2022-01-29 13:30:18 +0530

    kth smallest

--- rough.py
+++ rough.py
@@ -2621,3 +2621,21 @@ class solution:
         return nums
 print(Solution().rotate([1,2,3,4,5], 2))
 
+def kthsmall(arr, l, r, k):
+    if (k >0, k <=r-l, + 1):
+        pos= partition(arr, l, r)
+        if (pos - l == k -1):
+            return arr[pos]
+        if (pos - l > k-1):
+            return kthsmall(arr, l, pos-1, k)
+        return kthsmall(arr, pos + 1, r, k-pos+l - 1)
+        
+def partition(arr, l, r):
+    x = arr[r]
+    i = l
+    for j in range(l, r):
+        if (arr[j] <= x):
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
