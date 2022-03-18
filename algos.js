3834d42794c428bee7152054f7aaf4cac4d8165d
axaysushir <akshay.sushir99@gmail.com>
2022-03-18 14:00:00 -0600

    Decode string

--- algos.js
+++ algos.js
@@ -4766,3 +4766,29 @@ function findConcat(words) {
   });
   return ans
 }
+
+
+function decodeString(s) {
+  const stack = []
+  for (let c of s) {
+    if (c !== ']') {
+      stack.push(c)
+      continue;
+    }
+    let cur = stack.pop()
+    let str = ''
+    while (cur !== '[') {
+      str = cur + str
+      cur = stack.pop()
+    }
+    let num = ''
+    cur = stack.pop()
+    while (!Number.isNaN(Number(cur))) {
+      num = cur + num
+      cur = stack.pop()
+    }
+    stack.push(cur)
+    stack.push(str.repeat(Number(num)))
+  }
+  return stack.join('')
+}
