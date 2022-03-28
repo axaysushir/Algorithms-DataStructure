dbd766f7b07d6ba137085b9f94c32fca35a590bf
axaysushir <akshay.sushir99@gmail.com>
2022-03-28 14:00:00 -0600

    Remove node

--- algos.js
+++ algos.js
@@ -4867,3 +4867,22 @@ function reverseLink(head) {
   }
   return head;
 }
+
+function removeNth(head, n) {
+  let listnode;
+  let dummy = new listNode(0);
+  dummy.next = head;
+  let length = 0, first = head;
+  while (first !== null) {
+    length++;
+    first = first.next;
+  }
+  length -= n;
+  first = dummy;
+  while (length > 0) {
+    length--;
+    first = first.next;
+  }
+  fist.next = first.next.next;
+  return dummy.next
+}
