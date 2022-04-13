0a169f70cb4271b340c8f33640b76e13435cc9b3
axaysushir <akshay.sushir99@gmail.com>
2022-04-13 20:25:14 +0530

    Permutation

--- pemutation.js
+++ pemutation.js
@@ -1,6 +1,6 @@
 function permutation(s, ans) {
   if (s.length == 0) return s
-  var rest;
+  var rest, char;
   for (let i=0; i<s.length; i++) {
     let char = s[i]
     let leftSubstr = s.slice(0, i)
@@ -12,3 +12,21 @@ function permutation(s, ans) {
 }
 
 console.log(permutation('abc', ''));
+
+const permute = (s) => {
+  if (s.length == 0 || s.length < 2) return s
+  let ans = []
+  for (let i=0; i<s.length; i++) {
+    let char = s[i]
+
+    // remove duplicates
+    if (s.indexOf(char) !== i) continue
+    let remChar = s.slice(0, i) + s.substr(i+1, s.length)
+    console.log('rem', remChar);
+    for (let item of permute(remChar)) {
+      ans.push(char + item)
+    }
+  }
+  return ans
+}
+console.log(permute('abc', ''));
