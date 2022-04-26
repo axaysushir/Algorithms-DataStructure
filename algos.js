a3f6b7be6492211daa6d0de41350876d97b8f410
axaysushir <akshay.sushir99@gmail.com>
2022-04-26 14:00:00 -0600

    LIS

--- algos.js
+++ algos.js
@@ -5394,3 +5394,18 @@ function title(s) {
   return res
 }
 console.log(title('AC'));
+
+var max = nums => {
+  const dp  = new Array(nums.length + 1)
+  dp.fill(1)
+  let max = 0;
+  for (let i=0; i<nums.length; i++) {
+    for (let j=0; j<i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j]+1)
+      }
+    }
+    max = Math.max(dp[i], max)
+  }
+  return max
+}
