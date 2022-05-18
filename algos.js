905d11eb208fdf2b9bf392d927cd7f65c3c4574c
axaysushir <akshay.sushir99@gmail.com>
2022-05-18 16:45:49 +0530

    add string

--- algos.js
+++ algos.js
@@ -5513,3 +5513,19 @@ var minJump = nums => {
   }
   return count
 }
+
+var addB = (a, b) => {
+  let i= a.length - 1, j = b.length-1;
+  let carry = 0, res = ''
+  while (i >= 0 || j >= 0) {
+    let sum = carry;
+    if (i >= 0) sum += (a[i]) - 0;
+    if (j >= 0) sum += (b[j]) - 0;
+    res = parseInt(sum % 2) + res
+    carry = parseInt(sum /2)
+    i--;
+    j--;
+  }
+  if (carry !== 0) res = 1 + res
+  return res
+}
