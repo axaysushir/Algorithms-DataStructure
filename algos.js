225f6aa28c6998ef16db536d1ca350f1b5cbf668
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-06-21 18:57:16 +0530

    islands

--- algos.js
+++ algos.js
@@ -5685,3 +5685,27 @@ let maxsub = nums => {
   }
   return global
 }
+
+function getIslandCount(grid, i, j) {
+  if (i <0 || i>= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') return 0;
+  grid[i][j] = '0';
+  getIslandcount(grid, i+1, j)
+  getIslandcount(grid, i-1, j)
+  getIslandCount(grid, i, j-1)
+  getIslandCount(grid, i, j+1)
+  return 1
+}
+
+const numisland = grid => {
+  if (grid == null || grid.length == 0) return 0;
+  let island = 0;
+
+  for (let i=0; i<grid.length; i++) {
+    for (let j=0; j<grid[i].length; j++) {
+      if (grid[i][j] == '1') {
+        island += getIslandCount(grid, i, j)
+      }
+    }
+  }
+  return island
+}
