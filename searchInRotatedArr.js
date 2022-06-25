45460f38b0fbf260822feaa744bc0c10cc48167c
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-06-25 16:31:32 +0530

    search in array

---
+++ searchInRotatedArr.js
@@ -0,0 +1,15 @@
+var search = function(nums, target) {
+    let left = 0, right = nums.length-1;
+    while (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        if (nums[mid] == target) return mid;
+        if (nums[mid] < nums[right]) {
+            if (target <= nums[right] && target > nums[mid]) left = mid + 1
+            else right = mid-1
+        } else {
+            if (target >= nums[left] && target > nums[mid]) right = mid-1
+            else left = mid+1
+        }
+    }
+    return -1
+}
