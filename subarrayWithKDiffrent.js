9dcdfb9c3da510ed32ae44e052b99ac2e005483e
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-07-30 20:48:05 +0530

    subarray sumk

--- subarrayWithKDiffrent.js
+++ subarrayWithKDiffrent.js
@@ -42,3 +42,16 @@ var subarraysWithKDistinct = function(nums, k) {
     }
     return atmost(k) - atmost(k-1)
 }
+
+var subarray = (nums, k) => {
+    let count = 0, sum = 0, map = new Map()
+    map.set(0,1)
+    for (let i=0; i<nums.length; i++) {
+        sum += nums[i]
+        let target = sum-k
+        if (map.has(target)) count += map.get(target)
+        if (!map.has(target)) map.set(sum, 0)
+        map.set(sum, map.get(sum) + 1)
+    }
+    return count
+}
