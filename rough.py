cfd096782e69ed097383da483f57f76744380f75
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-08-03 19:34:12 +0530

    char map

--- rough.py
+++ rough.py
@@ -3050,3 +3050,17 @@ def calculate(root: Node):
         return root.key
     else:
         return calculate(evaluate(root.left), root.key, evaluate(root.right))
+
+
+def has_character_map(s, t):
+    strdict = {}
+    for i in range(len(s)):
+        if (s[i] not in strdict.keys()):
+            if t[i] not in strdict.values():
+                strdict[s[i]] = t[i]
+            else:
+                return False
+        else:
+            if strdict[s[i]] = t[i]:
+                return False
+    return True
