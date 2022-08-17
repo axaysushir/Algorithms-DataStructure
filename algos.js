b2d4eb3d02285ea22a07d327f298920d43f439a0
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-08-17 17:08:01 +0530

    search in arr

--- algos.js
+++ algos.js
@@ -6047,3 +6047,20 @@ var maxSub = nums => {
   return global
 }
 console.log(maxSub([1, 2, -3]));
+
+var searchINArr = (nums, target) => {
+  let left= 0, right = nums.length-1;
+
+  while (left <= right) {
+    let mid = Math.floor((left+right)/2)
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < nums[right]) {
+      if (target <= nums[right] && target > nums[mid]) left = mid + 1
+      else right = mid - 1
+    } else {
+      if (target >= nums[left] && target > nums[mid]) right = mid-1
+      else left = mid + 1
+    }
+  }
+  return -1
+}
