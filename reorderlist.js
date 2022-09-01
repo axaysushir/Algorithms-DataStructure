f84164d4bac9a6f20ee94ca5eba4e55c020c1a83
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-09-01 18:51:43 +0530

    reorder list

---
+++ reorderlist.js
@@ -0,0 +1,34 @@
+function reorderList(head) {
+    let fast = slow = head
+
+    while (fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next
+    }
+    // store second half
+    let second = slow.next
+    // make last node in left half equal to null
+    slow.next = null;
+
+    // rev right half
+    let prev = null
+    while (second) {
+        let temp = second.next
+        second.next = prev
+        prev = second
+        second = temp
+    }
+
+    let left = head, right = prev
+    // alternate node
+    while (right && left) {
+        let templeft = left.next
+        let tempright = right.next
+
+        left.next = right
+        right.next = templeft
+
+        left = templeft
+        right = tempright
+    }
+}
