92e22b440ed157377b0015dcb8edfd8538b4b4d0
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-09-03 17:01:35 +0530

    palindrome

--- algos.js
+++ algos.js
@@ -6181,3 +6181,27 @@ var validparent = str => {
     if (stack.length == 0) return true
     return false
 }
+
+const expand = (s, left, right) => {
+  while (left >= 0 && right < s.length && s.charAt(left) == s.charAt(right)) {
+    left--
+    right++
+  }
+  return right - left - 1
+}
+
+var palin = str => {
+  if (s.length < 1 || s == null) return false
+  let start = 0, maxlen = 0
+  for (let i=0; i<s.length; i++) {
+    let len1 = expandAroundCenter(s, i, i)
+    let len2 = expandAroundCenter(s, i, i+1)
+    let len = Math.max(len1, len2)
+
+    if (len > maxlen) {
+      maxlen = len
+      start = i - Math.floor((len-1)/2)
+    }
+  }
+  return str.substr(len, maxlen)
+}
