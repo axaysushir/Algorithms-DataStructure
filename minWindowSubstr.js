a316dcce743a6ccf04c4c98bc8d2defe62063280
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-09-08 17:59:53 +0530

    window substr

--- minWindowSubstr.js
+++ minWindowSubstr.js
@@ -1,3 +1,18 @@
+/* Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+The testcases will be generated such that the answer is unique.
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+Example 2:
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+*/
+
 function minWindow(s, t) {
     let hash = {}
     // add all ele to hash table
