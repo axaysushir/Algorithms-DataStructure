db55a75ce76b220021248be10dc09a2847940da4
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-09-16 17:56:50 +0530

    two sum

--- algos.js
+++ algos.js
@@ -6341,3 +6341,14 @@ const maxsubarrsum = nums => {
   }
   return global
 }
+
+const twosums = (nums, target) => {
+  let l = 0, r = nums.length - 1
+  while ( l< r) {
+    let sum = nums[l] + nums[r]
+    if (sum == target) {
+      return [l+1, r+1]
+    } else if (sum > target) r--
+    else l++
+  }
+}
