45b7633061483e34c9f822c72f04031224e291bb
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-09-17 15:53:28 +0530

    threes sum

--- algos.js
+++ algos.js
@@ -6352,3 +6352,30 @@ const twosums = (nums, target) => {
     else l++
   }
 }
+
+const threesum = nums => {
+  let target = 0, res = []
+
+  if (nums.length < 3) return res
+  nums.sort((a, b) => a-b)
+  for (let i=0; i<nums.length-2; i++) {
+    if (nums[i] > target) break
+    if (i > 0 && nums[i] == nums[i-1]) continue
+    let j = i + 1
+    let k = nums.length - 1
+    while (j < k) {
+      let sum = nums[i] + nums[j] +nums[k]
+      if (sum == target) {
+        res.push([nums[i], nums[j], nums[k]])
+        while (nums[j] == nums[j+1]) j++
+        while (nums[k] == nums[k-1]) k--
+        j++
+        k--
+        continue
+      }
+      if (sum > target) k--
+      if (sum < target) j++
+    }
+  }
+  return res
+}
