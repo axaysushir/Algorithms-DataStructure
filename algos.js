82df17f0297c95078da42842ff86d6ba26f13f04
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-10-31 18:42:50 +0530

    del dupli node

--- algos.js
+++ algos.js
@@ -6725,3 +6725,20 @@ function duplnode(head) {
   ans.next  = null
   return node.next
 }
+
+function deldupli(head) {
+  if (!head) return null
+  let pre = new ListNode(0)
+  pre.next = head;
+  let node = pre
+
+  while (node.mext && node.next.next) {
+    if (node.next.val == node.next.next.val) {
+      let val = node.next.val
+      while (node.next && node.next.val == val) {
+        node.next = node.next.next
+      }
+    } else node = node.next
+  }
+  return pre.next
+}
