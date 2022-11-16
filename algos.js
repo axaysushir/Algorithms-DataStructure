777cb4e96a8c10653cbc8d21dbdee5d07b1a6365
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-11-16 19:05:13 +0530

    add binary str

--- algos.js
+++ algos.js
@@ -6799,3 +6799,20 @@ function exist(board, word) {
   return false
 }
 console.log(exist(board, 'FALL'));
+
+function addBinary(a,b) {
+  let i = a.length - 1
+  let j = b.length - 1
+  let carry = 0, res = ''
+  while (i >= 0 || j >= 0) {
+    let sum = carry
+    if (i >= 0) sum += a[i] - 0
+    if (j >= 0) sum += b[j] - 0
+    res = parseInt(sum % 2) + res
+    carry = parseInt(sum/2)
+    i--
+    j--
+  }
+  if (carry != 0) res+= 1
+  return res
+}
