1def726d2a9d6a64c96dcd8717aede052bd4d6ba
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2022-12-25 18:26:24 +0530

    boats to save people

--- algos.js
+++ algos.js
@@ -6955,3 +6955,16 @@ function bud(a, b) {
   }
   return count == 2 ? true : false
 }
+
+function numOfboats(people, limit){
+  people.sort((a,b) => a-b)
+  let i=0, j = people.length -1, boats = 0
+  while (i <= j) {
+    if (people[i]+people[j] <= limit) {
+      i++
+      j++
+    } else j--
+    boats++
+  }
+  return boats
+}
