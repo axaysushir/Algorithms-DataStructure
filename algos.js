09834a3dfab847e5d40f931bc2b89f6b4e1c9870
Axay Sushir <51092842+axaysushir@users.noreply.github.com>
2023-01-13 16:22:31 +0530

    subarr

--- algos.js
+++ algos.js
@@ -7068,3 +7068,16 @@ function maxSubarr(nums) {
   }
   return maxglo
 }
+
+function subarr(nums, k) {
+  let count = 0, sum = 0, map = new Map()
+  map.set(0, 1)
+  for (let i=0; i<nums.length; i++) {
+    sum += nums[i]
+    let target = sum - k;
+    if (map.has(target)) countn += map.get(target)
+    if (!map.has(target)) map.set(sum ,0)
+    map.set(sum, map.get(sum)+1)
+  }
+  return count
+}
